b5ee646dc6c2e8f7c7d5bdadf8503ae0af41bcf2
Gabriel Baltar <55629378+gabrielbaltar@users.noreply.github.com>
2024-08-10 18:08:08 -0300

    feat: contact page form

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -3,11 +3,59 @@ import './styles.css';
 const Contact = () => { 
 
   return (
-    <section className='contact-container'>
+    <section className="contact-container">
+      <h1>Touch base via form or connect directly</h1>
+      <div className='form-container'>
       <div className="contact">
-        <h1>
-          Touch base via form or connect directly
-        </h1>
+        <div className="contact-number">
+          <span>Telefone</span>
+          <p>+21 98909 0580</p>
+        </div>
+        <div className="contact-place">
+          <span>If you wish for a face-to-face discussion, find us at our office location</span>
+          <p>123 Artistic Lane, Los Angeles, CA 90001, U.S.A.</p>
+        </div>
+        <p>
+          Share your ideas or aspirations with us, we're all ears. Our attentive
+          team is keen to understand, appreciate, and aid in bringing your
+          vision into reality.
+        </p>
+      </div>
+      <div className="contact-form">
+        <form>
+          <div className='contact-input'>
+            <label htmlFor="name">Nome</label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Digite seu nome"
+              required
+            />
+          </div>
+          <div className='contact-input'>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Digite seu email"
+              required
+            />
+          </div>
+          <div className='contact-input'>
+            <label htmlFor="message">Mensagem</label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder="Digite sua mensagem"
+              required
+            />
+          </div>
+
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
       </div>
     </section>
   );
