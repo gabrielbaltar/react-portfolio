9275b64651af031c9eacf236509ca5dcfb3b1f84
gabrielbaltar <201902046293@alunos.estacio.br>
2024-09-20 04:15:09 -0300

    feat: add new page: 'Project Details'

---
+++ src/pages/ProjectDetails/projectsDetails.jsx
@@ -0,0 +1,15 @@
+import 'style.css';
+
+const ProjectDetails = () => { 
+
+    // Aqui será a página de detalhes do projeto
+
+    return (
+
+        <h2>Olá, Mundo!</h2>
+        
+    )
+
+}
+
+export default ProjectDetails;
